ea66e247b7fb9f9425e550dfb079f1e3ef1a534c
Raj Agrahari <raj.agr2002@gmail.com>
2025-10-03 22:23:31 +0530

    JS-Random-Background-Generator: Highlights the technology (JavaScript) and the function.

---
+++ Day 13/index.js
@@ -0,0 +1,60 @@
+// //! reduce 
+
+// let arr = [10,20, 30, 40];
+// let sum = arr.reduce((acc, el, i, arr)=>{
+//     return acc + el;
+// })
+
+// let prod = arr.reduce((acc, el, i, arr)=>{
+//     return acc * el;
+// })
+
+// let temp = arr.reduce((acc, el, i, arr)=>{
+//     console.log(acc, el);
+    
+//     return acc + el;
+// })
+// // ! after passing the initalvalue
+// let summ = arr.reduce((acc, el, i, arr)=>{
+//      console.log(acc, el);
+//     return acc + el;
+// },500)
+
+// console.log(sum);
+// console.log(prod);
+// console.log(temp);
+// console.log(summ);
+
+// console.log("======================================================================")
+// // ! reduceRight
+
+// let summm = arr.reduceRight((acc, el, i, arr)=>{
+//      console.log(acc, el);
+//     return acc + el;
+// },500)
+// console.log(summm);
+
+
+
+
+// // ! reverse a stirng
+// let str = 'hey hello hii'
+// let reverse = str.split(' ').reduceRight((acc, el, i)=>{
+//      console.log(acc, el);
+//     return acc+' '+el;
+// })
+// console.log(reverse);
+
+
+
+console.log("==================================================================");
+
+
+//!!!!! return the longest word in a string using reduce
+
+let str1 = 'this is a javascript we are learning' 
+let word  = str1.split(' ');
+let output = word.reduce((acc, el)=>{
+    return acc.length > el.length ? acc : el;
+}) 
+console.log(output);
